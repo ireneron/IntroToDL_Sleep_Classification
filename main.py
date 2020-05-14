5c75aad81643190092d7cb5107e9064bae72d64a
Ronja <r.g.i.ronnback@tilburguniversity.edu>
2020-05-14 14:33:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ Created on Sat Apr 11 11:17:17 2020
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sn
+import pydot
 
 from scipy.signal import spectrogram
 
@@ -17,12 +19,18 @@ from keras.models import Sequential, Model
 from keras.layers import Dense, LSTM, Flatten, TimeDistributed, Conv1D, Conv2D
 from keras.layers import Dropout, GlobalAveragePooling1D, MaxPooling1D
 from keras.layers import MaxPooling2D, concatenate, Input, Concatenate
-from keras.layers import Activation, Reshape, BatchNormalization
+from keras.layers import Activation, Reshape, BatchNormalization, GRU
 from keras.layers.embeddings import Embedding
 from keras.optimizers import Adam
 from keras.utils import to_categorical
 
+from keras.utils.vis_utils import plot_model
 
+from sklearn.metrics import confusion_matrix
+
+# =============================================================================
+# TRAINING DATA
+# =============================================================================
 path = "/Users/ronjaronnback/Desktop/University/Year_3/Semester 2/Introduction to Deep Learning/Assignment/Data_Raw_signals.pkl"
 raw_train_data, test_data = pd.read_pickle(path)
 
@@ -33,6 +41,12 @@ y = test_data
 
 frequencies = np.asarray((unique, counts)).T
 print(frequencies)
+
+# =============================================================================
+# FINAL TESTING
+# =============================================================================
+path = "/Users/ronjaronnback/Desktop/University/Year_3/Semester 2/Introduction to Deep Learning/Assignment/Test_Raw_signals_no_labels.pkl"
+test_raw = pd.read_pickle(path)
 
 #------------------------------------------------------------------------------
 # MAKE SPECTROGRAMS
@@ -47,38 +61,38 @@ def create_spectrograms(data):
     return np.array(spectograms), freq, time
 
 train_data, frequencies, times = create_spectrograms(raw_train_data)
+final_test, frequencies, times = create_spectrograms(test_raw)
 
 # 15000 samples, 2 spots on head, 100 Hz for 30 seconds
 print(train_data.shape)
 print(test_data.shape)
+print(final_test.shape)
 # should it be remodelled to 15375, 100, 30, 2??
+
+final_test = final_test.reshape(1754, 1, 101, 30)
+final_test = final_test.reshape(1754, 101, 30, 1)
 
 train_data = train_data.reshape(15375, 100, 30, 2)
 
-del X, y, unique, counts, path, raw_train_data
+del X, y, unique, counts, path, raw_train_data, test_raw, times, frequencies
 
-#------------------------------------------------------------------------------
+# =============================================================================
 # TRAIN TEST SPLIT
-#------------------------------------------------------------------------------
+# =============================================================================
 from sklearn.model_selection import train_test_split
 
 
 X_train, X_test, Y_train, Y_test = train_test_split(train_data, 
                                                     test_data, 
-                                                    test_size=0.33, 
+                                                    test_size=0.2, 
                                                     random_state=42)
 
-X_train_left = X_train[:,:,:,0]
-X_train_right = X_train[:,:,:,1]
-
-X_test_left = X_test[:,:,:,0]
-X_test_right = X_test[:,:,:,1]
 
 #https://stackoverflow.com/questions/46443566/keras-lstm-multiclass-classification
 Y_train = to_categorical(Y_train)
 Y_test = to_categorical(Y_test)
 
-del frequencies, times, test_data, train_data
+del test_data, train_data
 
 #------------------------------------------------------------------------------
 # LSTM MODEL
@@ -92,7 +106,7 @@ Best Performance: 0.81
 - optimizer = adam
 - dropout = 0.2
 
-Other Try:
+Other Try: 0.81 as well
 - neurons = 100
 - loss = 'kullback_leibler_divergence'
 - optimizer = nadam
@@ -103,155 +117,78 @@ https://github.com/CVxTz/EEG_classification/blob/f440a7f077d3dcb7c5ed5a5688ae62e
 
 """
 
+verbose = 1
+epochs = 25
+batch_size = 100
 
-def evaluate_lstm_model(trainX, trainy, testX, testy, epochs = 20, batch_size = 20):
-    verbose = 1
-    epochs = epochs
-    batch_size = batch_size
-    
-    height = trainX.shape[1]
-    width = trainX.shape[2]
-    depth = trainX.shape[3]
-    n_outputs = trainy.shape[1]
+height = X_train.shape[1]
+width = X_train.shape[2]
+depth = X_train.shape[3]
+n_outputs = Y_train.shape[1]
 
-    
-    lstm_model = Sequential()
-    lstm_model.add(TimeDistributed(Flatten(input_shape=(width,depth)))) # https://stackoverflow.com/questions/52936132/4d-input-in-lstm-layer-in-keras
-    lstm_model.add(LSTM(100))
-    lstm_model.add(Dropout(0.2))
-    lstm_model.add(Dense(n_outputs, activation='softmax'))
-    lstm_model.compile(loss='kullback_leibler_divergence', optimizer='nadam', metrics=['accuracy'])
-    #print(lstm_model.summary())
-    
-    # loss 'categorical_crossentropy', 'kullback_leibler_divergence'
-    # opt 'Nadam', 'Adamax', 'Adadelta'
-    
-    # fit network
-    lstm_model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
-	
-    # evaluate model
-    _, accuracy = lstm_model.evaluate(testX, testy, batch_size=batch_size, verbose=1)
-    return accuracy
+lstm_model = Sequential()
+lstm_model.add(TimeDistributed(Flatten(input_shape=(width,depth)))) # https://stackoverflow.com/questions/52936132/4d-input-in-lstm-layer-in-keras
+lstm_model.add(GRU(200))
+lstm_model.add(Dropout(0.5))
+#lstm_model.add(Dense(50))
+#lstm_model.add(Dropout(0.5))
+lstm_model.add(Dense(n_outputs, activation='softmax'))
+lstm_model.compile(loss='kullback_leibler_divergence', optimizer='nadam', metrics=['accuracy'])
 
-evaluate_lstm_model(X_train, Y_train, X_test, Y_test)
-
-
-#------------------------------------------------------------------------------
-# CNN MODEL - PERFORMS BETTER
-#------------------------------------------------------------------------------
-"""
-Source : https://machinelearningmastery.com/cnn-models-for-human-activity-recognition-time-series-classification/
-
-Best Performance: 0.59
-
-"""
-
-def evaluate_cnn_model(trainX, trainy, testX, testy):
-    verbose = 1
-    epochs = 20
-    batch_size = 20
-    
-    height = trainX.shape[1]
-    width = trainX.shape[2]
-    depth = trainX.shape[3]
-    n_outputs = trainy.shape[1]
-    
-    model = Sequential()
-    model.add(Conv2D(filters=64, kernel_size=3, activation='relu', input_shape=(height, width, depth)))
-    model.add(Conv2D(filters=64, kernel_size=3, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(MaxPooling2D(pool_size=2))
-    model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(n_outputs, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # loss 'sparse_categorical_crossentropy', 'kullback_leibler_divergence'
-    # opt 'Nadam', 'Adamax', 'Adadelta'
-    
-    # fit network
-    model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
-	
-    # evaluate model
-    _, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
-    return accuracy
-
-evaluate_cnn_model(X_train, Y_train, X_test, Y_test)
-
-
-
-#------------------------------------------------------------------------------
-# CHANNEL SPLIT MODEL
-#------------------------------------------------------------------------------
-
-# define two sets of inputs
-inputLeft = Input(shape=(100,30))
-inputRight = Input(shape=(100,30))
-
-# the first branch operates on the first input
-left = Conv1D(filters=64, kernel_size=3, activation='relu')(inputLeft)
-left = Conv1D(filters=64, kernel_size=3, activation='relu')(left)
-left = Dropout(0.5)(left)
-left = MaxPooling1D(pool_size=2)(left)
-left = Flatten()(left)
-left = Dense(100, activation='relu')(left)
-left = Model(inputs=inputLeft, outputs=left)
-
-# the second branch opreates on the second input
-right = Conv1D(filters=64, kernel_size=3, activation='relu')(inputRight)
-right = Conv1D(filters=64, kernel_size=3, activation='relu')(right)
-right = Dropout(0.5)(right)
-right = MaxPooling1D(pool_size=2)(right)
-right = Flatten()(right)
-right = Dense(100, activation='relu')(right)
-right = Model(inputs=inputRight, outputs=right)
-
-# combine the output of the two branches
-combined = concatenate([left.output, right.output])
-
-# apply a FC layer and then a regression prediction on the
-# combined outputs
-final = Dense(10, activation="relu")(combined)
-final = Dense(6, activation="linear")(final)
-# our model will accept the inputs of the two branches and
-# then output a single value
-model = Model(inputs=[left.input, right.input], outputs = final)
-
-print(model.summary)
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# loss 'categorical_crossentropy', 'kullback_leibler_divergence'
+# opt 'Nadam', 'Adamax', 'Adadelta'
 
 # fit network
-model.fit([X_train_left, X_train_right], Y_train, epochs=50, batch_size=20, verbose=2)
+history = lstm_model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size, verbose=verbose, validation_data = (X_test, Y_test))
+print(lstm_model.summary())
+
+#plot training and validation accuracy
+plt.figure(0)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+
+# =============================================================================
+# 
+# =============================================================================
 
 # evaluate model
-model.evaluate([X_test_left, X_train_right], Y_test, batch_size=20, verbose=2)
+lstm_model.evaluate(X_test, Y_test, batch_size=batch_size, verbose=1)
+
+#plot confusion matrix
+y_pred = lstm_model.predict_on_batch(final_test)
+cm = confusion_matrix(np.argmax(Y_test, axis = 1), np.argmax(y_pred, axis=1))
+df_cm = pd.DataFrame(cm, range(6), range(6))
+
+plt.figure(1)
+sn.set(font_scale=1.2) # for label size
+sn.heatmap(df_cm, annot=True, annot_kws={"size": 10},  fmt='g') # font size
+plt.show()
 
 
 
-
-"""
-# THIS ONE WORKS BUT ONLY TAKES ONE CHANNEL INPUT
-def evaluate_model(trainX, trainy, testX, testy):
-	verbose, epochs, batch_size = 1, 10, 32
-	n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
-    
-	model = Sequential()
-	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps,n_features)))
-	model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(MaxPooling1D(pool_size=2))
-	model.add(Flatten())
-	model.add(Dense(100, activation='relu'))
-	model.add(Dense(n_outputs, activation='softmax'))
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	# fit network
-	model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
-	# evaluate model
-	_, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
-	return accuracy
+#export answers
 
 
-evaluate_model(X_train_left, Y_train, X_test_left, Y_test)
-"""
-   
-   
+final_predictions = np.argmax(y_pred, axis=1)
+
+a_file = open("/Users/ronjaronnback/Desktop/answer.txt", "w")
+np.savetxt(a_file, final_predictions, fmt = '%d')
+a_file.close()
+
+# =============================================================================
+# Save and Load Model
+# =============================================================================
+# save model and architecture to single file
+lstm_model.save("/Users/ronjaronnback/Desktop/model.h5")
+print("Saved model to disk")
+
+from keras.models import load_model
+# load model
+model = load_model('/Users/ronjaronnback/Desktop/model.h5')
+# summarize model.
+model.summary()
